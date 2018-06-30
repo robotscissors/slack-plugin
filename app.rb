90e394e4465f312491f7f9a693a27cdb57b945a3
robotscissors <cjbazin@gmail.com>
2018-06-30 12:11:03 -0700

    Add help information

--- app.rb
+++ app.rb
@@ -12,7 +12,6 @@ get '/' do
 end
 
 ERROR = ':boom: Error. I am sure it isn\' anything serious, but I am not sure what command you were trying. Additional commands you can use are: start, stop or restart just leave it blank for stats.'
-
 post '/slack/command' do
   @slack_user = params #get the slack ID unique identifier
   @user = User.find_by(slack_identifier: @slack_user[:user_id]) #does the user exist?
@@ -97,7 +96,17 @@ post '/slack/command' do
       ]
     }.to_json
   when 'help' #give stats to the user
-    "Will give you the help"
+    content_type :json
+    {
+      :response_type => "ephemeral",
+      :text => ":heart: Help is on the way!",
+      :attachments =>[
+         {
+            :text => "Here are some commands you can use: \n```/slacktrack``` -> by itself will send you a current log report. \n\n```/slacktrack start``` -> will start the timer. \n\n```/slacktrack stop``` -> will stop the timer. \n\n```/slacktrack restart``` -> will restart the timer for the current session. \n\n```/slacktrack clear``` -> will reset the log for the current user.",
+            :color => "#f4e242"
+         }
+     ]
+   }.to_json
   else
     content_type :json
     {:text => "#{ERROR}"}.to_json
