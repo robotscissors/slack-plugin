c86203913327763eb78c4abe8c42134d28dd5b43
robotscissors <cjbazin@gmail.com>
2018-06-30 10:33:48 -0700

    Add time calculation

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'time_difference'
 
 require './models/user'
 require './models/activities'
 require './models/slack'
 require 'pry-byebug'
 
+
 get '/' do
   'Hello there, slack user!!'
 end
 
-ERROR = 'Error. I am sure it isn\' anything serious, but I am not sure what command you were trying. Additional commands you can add: start, stop or just leave it blank for stats.'
+ERROR = ':boom: Error. I am sure it isn\' anything serious, but I am not sure what command you were trying. Additional commands you can use are: start, stop or restart just leave it blank for stats.'
 
 post '/slack/command' do
   @slack_user = params #get the slack ID unique identifier
@@ -18,37 +20,33 @@ post '/slack/command' do
   ## find out what the user wants to do
   case params[:text].to_s.strip.downcase
   when '' #user just wants any stats and the current state
-    "nothing there"
+    message = Activities.get_time_log(@slack_user)
+    "#{message}"
   when 'start' #user wants to mark thst start of the clock
     if @user #does the user already have clock starting
-      if Activities.start(@slack_user)
-        content_type :json
-        {:text => "OK, Let's start the clock"}.to_json
-      else
-        "There was an error please try again"
-      end
+      message = Activities.start(@slack_user)
+      content_type :json
+      {:text => "#{message}"}.to_json
     else #wait the clock is still running - help the user
       "The clock is already ticking!"
     end
   when 'stop' #stop the clock we are done
     if @user #check to make sure the timer has started
-      if Activities.stop(@slack_user)
-        "TIME! Ok, stopped the clock."
-      else
-        "There is a problem, please try again."
-      end
+      message = Activities.stop(@slack_user)
+      content_type :json
+      {:text => "#{message}"}.to_json
     else #oops they need to start the timer before they can stop it -help user
-      "You need to start the timer first"
+      content_type :json
+      {:text => "You need to start the timer first using ```/SlackTrack start```"}.to_json
     end
   when 'restart' #give stats to the user
-    if Activities.restart(@slack_user)
-      "The clock has been reset"
-    else
-      "Either there was a problem or you haven't started a session yet."
-    end
-  when 'time' #give stats to the user
-    "Will give you the time spent."
+    message = Activities.restart(@slack_user)
+    content_type :json
+    {:text => "#{message}"}.to_json
+  when 'help' #give stats to the user
+    "Will give you the help"
   else
-    ERROR
+    content_type :json
+    {:text => "#{ERROR}"}.to_json
   end
 end
--- models/activities.rb
+++ models/activities.rb
@@ -1,25 +1,61 @@
+# This class contains all the activites associated with a slack user
+# it calculates start and stop time differences, produces the log
+# and inserts items into the user database.
+
 class Activities < ActiveRecord::Base
 
   def self.start(slack_user)
+    #check to see what the last command was if it was start we should let the user know
+    @last_activity = User.where(slack_identifier: slack_user[:user_id]).last.slack_activity
+    return "You already started the clock" if @last_activity === 'start'
     #create the new user log
     activity = "start"
-    self.update(slack_user, activity)
+    if self.update(slack_user, activity)
+      "Time starts now! Go get'em"
+    else
+      "There was an error :boom:"
+    end
   end
 
   def self.stop(slack_user)
+    #check to see what the last command was if it was stop we should let the user know
+    @last_activity = User.where(slack_identifier: slack_user[:user_id]).last.slack_activity
+    return "You already stopped the clock" if @last_activity === 'stop'
     #create the new user log
     activity = "stop"
-    self.update(slack_user, activity)
+    if self.update(slack_user, activity)
+      "The clock has been stopped. #{self.get_time_log(slack_user)}"
+    else
+      "There was an error, please try again."
+    end
   end
 
   def self.restart(slack_user)
     #lets make sure the clock needs to be reset
-    @last = User.find_by(slack_identifier: slack_user[:user_id])
+    @last = User.where(slack_identifier: slack_user[:user_id]).last
     if @last.slack_activity === "start"
       activity = "start"
-      return self.update(slack_user, activity) #reset clock
+      if self.update(slack_user, activity) #reset clock
+        "The clock has been reset! Get busy!"
+      end
     end
-    false
+    "It looks like the clock hasn't started yet. Use /SlackTrack start command"
+  end
+
+  def self.get_time_log(slack_user)
+    #get the last start command that was issued - that is the start_time
+    @last_start_time = User.where(slack_identifier: slack_user[:user_id], slack_activity: "start").last.created_at
+    return false unless @last_start_time
+    #what was the last command that was executed?
+    @last_stop_time = User.where(slack_identifier: slack_user[:user_id], slack_activity: "stop").last.created_at
+    #if there is no stop yet, or the stop time was before the start time
+    return "You're still on the clock! So far you spent: #{get_time_duration(@last_start_time,Time.now)}" if !@last_stop_time || (@last_stop_time < @last_start_time)
+    #otherwise there was a full session and you should report it
+    "Your total slack time for the last session was: #{get_time_duration(@last_start_time,@last_stop_time)}"
+  end
+
+  def self.get_time_duration(start_time, end_time)
+    TimeDifference.between(start_time, end_time).humanize
   end
 
   private
