02433d909efc3e4dd1a6666cb2b174e4aaa80a69
robotscissors <cjbazin@gmail.com>
2018-06-30 08:29:48 -0700

    Create restart parameter

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ post '/slack/command' do
   when '' #user just wants any stats and the current state
     "nothing there"
   when 'start' #user wants to mark thst start of the clock
-    if !@user #does the user already have clock starting
+    if @user #does the user already have clock starting
       if Activities.start(@slack_user)
         content_type :json
         {:text => "OK, Let's start the clock"}.to_json
@@ -39,6 +39,12 @@ post '/slack/command' do
       end
     else #oops they need to start the timer before they can stop it -help user
       "You need to start the timer first"
+    end
+  when 'restart' #give stats to the user
+    if Activities.restart(@slack_user)
+      "The clock has been reset"
+    else
+      "Either there was a problem or you haven't started a session yet."
     end
   when 'time' #give stats to the user
     "Will give you the time spent."
--- models/activities.rb
+++ models/activities.rb
@@ -2,18 +2,30 @@ class Activities < ActiveRecord::Base
 
   def self.start(slack_user)
     #create the new user log
-    self.update(slack_user)
+    activity = "start"
+    self.update(slack_user, activity)
   end
 
   def self.stop(slack_user)
     #create the new user log
-    self.update(slack_user)
+    activity = "stop"
+    self.update(slack_user, activity)
+  end
+
+  def self.restart(slack_user)
+    #lets make sure the clock needs to be reset
+    @last = User.find_by(slack_identifier: slack_user[:user_id])
+    if @last.slack_activity === "start"
+      activity = "start"
+      return self.update(slack_user, activity) #reset clock
+    end
+    false
   end
 
   private
 
-  def self.update(slack_user)
-    @new_entry = User.create(slack_identifier: slack_user[:user_id], slack_command: slack_user[:command], slack_activity: slack_user[:text].downcase)
+  def self.update(slack_user, activity)
+    @new_entry = User.create(slack_identifier: slack_user[:user_id], slack_command: slack_user[:command], slack_activity: activity)
     return true if @new_entry.save #let's save the new Record return true if no errors
     false
   end
